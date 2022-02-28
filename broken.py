be9b7384e05ed0525fd8a91de576ad250d92f212
ecdrew1994 <73485381+ecdrew1994@users.noreply.github.com>
2022-02-28 14:45:10 -0700

    added changes to code
    removed error on float from floata to float.

---
+++ broken.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+sum = float(num1) + float(num2)
+
+print('input 1 {0}')
+print('input 2 {1}')
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
